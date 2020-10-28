19d4606535aff233a9e8a9db4e9a713c6e5be1ef
lucashbarbosa <luu.cashbarbosa@gmail.com>
2020-10-28 15:52:01 -0300

    Challenge 2 completed

---
+++ src/main/java/br/com/sithChallenge/model/Authors.java
@@ -0,0 +1,29 @@
+package br.com.sithChallenge.model;
+
+
+
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class AuthorsModel {
+    private String name;
+    private Date birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
